8632203f53d691bdb4439b1c37f6f237b5a0187c
merlin <krg25@njit.edu>
2019-08-05 19:27:04 -0400

    Added answer features

--- tests/Unit/userTest.php
+++ tests/Unit/userTest.php
@@ -30,4 +30,9 @@ class userTest extends TestCase
         $user = factory(\App\User::class)->make();
         $this->assertTrue(is_object($user->profile()->get()));
     }
+    public function testAnswers()
+    {
+        $user = factory(\App\User::class)->make();
+        $this->assertTrue(is_object($user->answers()->get()));
+    }
 }
